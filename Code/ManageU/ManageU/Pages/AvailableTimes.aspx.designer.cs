62fb487661dc86a2677bad3ec7924774e8b5456d
Andrew McCracken <Andrew McCracken>
2017-04-03 18:32:07 -0400

    Available time div now clickable

--- Code/ManageU/ManageU/Pages/AvailableTimes.aspx.designer.cs
+++ Code/ManageU/ManageU/Pages/AvailableTimes.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace ManageU.Pages {
     public partial class AvailableTimes {
         
         /// <summary>
+        /// btnHidden control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnHidden;
+        
+        /// <summary>
         /// displayTimesDiv control.
         /// </summary>
         /// <remarks>
